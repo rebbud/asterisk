8e1874427d731bb1e82d6345e20769d3099cd621
Hari Radhakrishnan <hari.radhakrishnan@dubber.net>
2018-02-12 04:35:49 +0000

    channel.h

--- include/asterisk/channel.h
+++ include/asterisk/channel.h
@@ -905,6 +905,8 @@ enum {
 	 * The data on chan->timingdata is an astobj2 object.
 	 */
 	AST_FLAG_TIMINGDATA_IS_AO2_OBJ = (1 << 23),
+
+	AST_FLAG_DUB_PAUSE_RESUME_RECORDING = (1 << 24),
 };
 
 /*! \brief ast_bridge_config flags */
